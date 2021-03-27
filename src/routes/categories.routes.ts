5bd39339ade1d0319723e333d111cdd750c6aed3
juanlessa <juanvlessa@ua.pt>
2021-03-27 14:10:44 +0000

    add categories post route

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express";
+
+const categoriesRoutes = Router();
+
+categoriesRoutes.post("/categories", (request: Request, response: Response) => {
+    const { name, description } = request.body;
+});
+
+export default categoriesRoutes;
